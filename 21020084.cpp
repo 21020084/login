23680957976e54c4706f5fc522fc020142408924
21020084 <21020084@vnu.edu.vn>
2022-02-23 08:49:59 +0700

    first commit

---
+++ 21020084.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<iomanip>
+#include<cmath>
+#include<algorithm>
+#include<string>
+using std::cin;
+using std::cout;
+using std::string;
+
+int main() {
+  cout << "Hello world!" << '\n';
+  cout << "Try to pull\n";
+
+  return 0;
+}
